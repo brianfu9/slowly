aff2bf9248c8e3ce59c2fff4efd1d9823c93def7
Brian <brianfu9@gmail.com>
2019-03-30 08:01:28 -0700

    i just want more commits

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" );
-})
 
 app.get('/api/v1/orders', function (req, res) {
    // Prepare output in JSON format

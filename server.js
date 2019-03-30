e168b550c25a7798876ed2a02c1ed297f5a49319
Brian <brianfu9@gmail.com>
2019-03-30 07:36:52 -0700

    reset base

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/api/v1/orders', function (req, res) {
+   // Prepare output in JSON format
+   console.log(req.params);
+   var response = {hello: 'world'};
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})

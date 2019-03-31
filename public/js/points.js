8ba506cf0c261d05e8001d84dceea87374e052ef
Bryant Bettencourt <bettencourt@berkeley.edu>
2019-03-30 19:41:03 -0700

    points stuff

--- public/js/points.js
+++ public/js/points.js
@@ -1,7 +1,11 @@
 (function($){
     var start_points = 0;
-    var start_odometer = 0
+    var odometer = $.get('http://localhost:3000/odometer', function(data) {});
+    var start_odometer = odometer["distance"];
+    
+
     function calculate_points(){
+        start_odometer 
 
     }
 }
